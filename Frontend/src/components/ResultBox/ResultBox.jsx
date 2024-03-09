e3e98c1774709ca1b712b3acd94aa28b5766b04f
Janith Sisitha <janithsisitha000.js@gmail.com>
2024-03-10 03:17:47 +0530

    change resultbox styles.

--- Frontend/src/components/ResultBox/ResultBox.jsx
+++ Frontend/src/components/ResultBox/ResultBox.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import './ResultBox.css'
 import Resultimg from '../../img/results-img.svg';
+import Resultimg02 from '../../img/pngwing7.svg';
 
 export default function ResultBox() {
   return (
     <div>
       <dir className="resultBox">
+        <div className="resultFlex">
         <div className="resultimg">
           <img src={Resultimg} alt="" />
         </div>
         <div className="resulttext">
             <h5>Your Result</h5>
         </div>
+        </div>
+         <div className="img02"><img src={Resultimg02} alt="" /> </div>
       </dir>
     </div>
   )
