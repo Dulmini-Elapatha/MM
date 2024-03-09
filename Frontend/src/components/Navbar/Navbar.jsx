c4e0d06fb146fd83218d0ded8a00c697ae1fb035
Janith Sisitha <janithsisitha000.js@gmail.com>
2024-03-10 03:31:45 +0530

    change user img in navbar.

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { /* Headless UI components */ } from '@headlessui/react';
 import { /* Heroicons components */ } from '@heroicons/react/24/outline';
 import Mindmatepng from '../../img/mindmate.svg'
+import Userimg from '../../img/userimg.svg'
 import './Navbar.css';
 
 
@@ -82,7 +83,7 @@ export default function Example() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={Userimg}
                         alt=""
                       />
                     </Menu.Button>
